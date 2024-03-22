b2f80edbb65c9ef920e663c3fa33ee59dd3f7b47
okan6848 <141925555+okan6848@users.noreply.github.com>
2024-03-22 18:03:46 +0300

    web

---
+++ app.js
@@ -0,0 +1,17 @@
+function gir() {
+  
+  window.location.href = 'index2.html';
+}
+const labels = document.querySelectorAll('.form-control label')
+
+labels.forEach((label) => {
+  label.innerHTML = label.innerText
+    .split('')
+    .map(
+      (letter, index) =>
+        `<span style="transition-delay:${index * 50}ms">${letter}</span>`
+    )
+    .join('')
+
+   
+})
